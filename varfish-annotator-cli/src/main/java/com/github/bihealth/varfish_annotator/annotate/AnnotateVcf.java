e34b38587b6e515be465b601414669096b7a7323
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2019-01-11 09:42:45 +0100

    Fixing priotization of annotated transcripts.

--- varfish-annotator-cli/src/main/java/com/github/bihealth/varfish_annotator/annotate/AnnotateVcf.java
+++ varfish-annotator-cli/src/main/java/com/github/bihealth/varfish_annotator/annotate/AnnotateVcf.java
@@ -559,19 +559,18 @@ public final class AnnotateVcf {
           .stream()
           .sorted(
               Comparator.<Annotation, VariantEffect>comparing(
-                      Annotation::getMostPathogenicVarType,
-                      (t1, t2) -> {
-                        if (t1 == null && t2 == null) {
-                          return 0;
-                        } else if (t2 == null) {
-                          return -1;
-                        } else if (t1 == null) {
-                          return 1;
-                        } else {
-                          return t1.compareTo(t2);
-                        }
-                      })
-                  .reversed())
+                  Annotation::getMostPathogenicVarType,
+                  (t1, t2) -> {
+                    if (t1 == null && t2 == null) {
+                      return 0;
+                    } else if (t2 == null) {
+                      return -1;
+                    } else if (t1 == null) {
+                      return 1;
+                    } else {
+                      return t1.compareTo(t2);
+                    }
+                  }))
           .collect(Collectors.toList());
     }
   }
