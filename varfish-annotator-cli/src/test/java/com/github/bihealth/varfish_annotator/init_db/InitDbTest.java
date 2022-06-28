d2e4b28f2a463875e80e6bd4adb22558cbaa790d
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2022-06-28 12:44:18 +0200

    Adding tests for db-stats (#31)
    Closes: #31
    Related-Issue: #31
    Projected-Results-Impact: none

--- varfish-annotator-cli/src/test/java/com/github/bihealth/varfish_annotator/init_db/InitDbTest.java
+++ varfish-annotator-cli/src/test/java/com/github/bihealth/varfish_annotator/init_db/InitDbTest.java
@@ -11,14 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 /**
- * Smoke test for the <code>init-db</code> sub command.
+ * Smoke tests for the <code>init-db</code> sub command.
  *
  * @author <a href="mailto:manuel.holtgrewe@bih-charite.de">Manuel Holtgrewe</a>
  */
 public class InitDbTest {
-
   @TempDir public File tmpFolder;
-
   File outputH2Db;
   File fastaFile;
   File faiFile;
@@ -75,16 +73,6 @@ public class InitDbTest {
     checkTable(tableName, expectedCount);
   }
 
-  @Test
-  void testClinvarImport37() throws SQLException {
-    testTsvImportImpl("Clinvar.tsv", "--clinvar-path", ClinvarImporter.TABLE_NAME, 1000);
-  }
-
-  @Test
-  void testHgmdImport37() throws SQLException {
-    testTsvImportImpl("HgmdPublicLocus.tsv", "--hgmd-public", HgmdPublicImporter.TABLE_NAME, 954);
-  }
-
   void testVcfImpl(String fileName, String argName, String tableName, int expectedCount)
       throws SQLException {
     final File vcfPath = new File(tmpFolder + "/" + fileName);
@@ -107,6 +95,16 @@ public class InitDbTest {
         });
 
     checkTable(tableName, expectedCount);
+  }
+
+  @Test
+  void testClinvarImport37() throws SQLException {
+    testTsvImportImpl("Clinvar.tsv", "--clinvar-path", ClinvarImporter.TABLE_NAME, 1000);
+  }
+
+  @Test
+  void testHgmdImport37() throws SQLException {
+    testTsvImportImpl("HgmdPublicLocus.tsv", "--hgmd-public", HgmdPublicImporter.TABLE_NAME, 954);
   }
 
   @Test
@@ -134,6 +132,29 @@ public class InitDbTest {
 
   @Test
   void testGnomadGenomesImport37() throws IOException, SQLException {
+    testVcfImpl(
+        "gnomad.genomes.r2.1.1.sites.chr1.stripped.vcf.bgz.gz",
+        "--gnomad-genomes-path",
+        GnomadGenomesImporter.TABLE_NAME,
+        1000);
+  }
+
+  /** The output of this test can be used to rebuild the <code>.h2.db</code> file. */
+  @Test
+  void testBuildFull37() throws SQLException {
+    testTsvImportImpl("Clinvar.tsv", "--clinvar-path", ClinvarImporter.TABLE_NAME, 1000);
+    testTsvImportImpl("HgmdPublicLocus.tsv", "--hgmd-public", HgmdPublicImporter.TABLE_NAME, 954);
+    testVcfImpl("ExAC.r1.sites.vep.vcf.gz", "--exac-path", ExacImporter.TABLE_NAME, 1000);
+    testVcfImpl(
+        "ALL.chr1.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz",
+        "--thousand-genomes-path",
+        ThousandGenomesImporter.TABLE_NAME,
+        1000);
+    testVcfImpl(
+        "gnomad.exomes.r2.1.1.sites.chr1.stripped.vcf.bgz.gz",
+        "--gnomad-exomes-path",
+        GnomadExomesImporter.TABLE_NAME,
+        1000);
     testVcfImpl(
         "gnomad.genomes.r2.1.1.sites.chr1.stripped.vcf.bgz.gz",
         "--gnomad-genomes-path",
