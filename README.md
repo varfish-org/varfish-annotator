c5078d3aff2d0711057f0411eea1a75c98ec4df5
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2022-09-12 16:45:45 +0200

    Properly document and test supported callers (#65)
    Closes: #63
    Related-Issue: #63
    Projected-Results-Impact: none

--- README.md
+++ README.md
@@ -14,6 +14,92 @@ Annotation of VCF file for import into VarFish (through Web UI).
 - thousand genomes
 - hgmd\_public from ENSEMBL r75
 
+## Supported Variant Callers
+
+VarFish annotator uses [HTSJDK](https://github.com/samtools/htsjdk) for reading [variant call format (VCF) files](https://samtools.github.io/hts-specs/).
+HTSJDK supports reading VCF v4.3 so the output of any tool that produces well-formed VCF can be read.
+VCF itself only specifies relatively few required fields and callers may use fields in a slightly different way.
+We thus document below what fields are used/interpreted by VarFish Annotator to prepare the files for VarFish.
+
+### Small Variants
+
+The following fields are considered:
+
+- `CHROM`
+  Will be written out in a normalized way, depending on the genome build.
+  That the `chr` prefix will be presenet for GRCh38 and it will be absent for GRCh37.
+- `POS`
+  The 1-based chromosomal position is written out.
+- `REF`
+  The reference allele will be written out.
+- `ALT`
+  For each alternative allele and each gene, a record is written out.
+  Asterisk alleles will be ignored ([see Broad's GATK documentation on this](https://gatk.broadinstitute.org/hc/en-us/articles/360035531912-Spanning-or-overlapping-deletions-allele-)).
+- `FORMAT` and per `SAMPLE`
+  - `GT` The genotype is written out with phasing information.
+  - `AD` The allelic depth of the alternative allele of the current record is written out.
+  - `DP` The total depth is written out to compute the allelic balance of the alternative allele of the current record is written out.
+  - `GQ` The genotype quality is written out.
+
+### Structural Variants / Copy Number Variants
+
+Note that if the `INFO/SVMETHOD` field is missing then you should define `--default-sv-method` as you would otherwise get a problem downstream.
+If you have any problem with your data then please tell us by opening a GitHub issue.
+
+The following fields are considered:
+
+- `CHROM`
+  Will be written out in a normalized way, depending on the genome build.
+  That the `chr` prefix will be presenet for GRCh38 and it will be absent for GRCh37.
+- `POS`
+  The 1-based chromosomal position is written out.
+- `REF`
+  The reference allele will be written out.
+- `ALT`
+  For each alternative allele and each gene, a record is written out.
+  Asterisk alleles will be ignored ([see Broad's GATK documentation on this](https://gatk.broadinstitute.org/hc/en-us/articles/360035531912-Spanning-or-overlapping-deletions-allele-)).
+- `INFO/CHR2`
+  Second chromosome of the SV, if not on the same chromosome.
+- `INFO/END`
+  End position of the SV.
+- `INFO/CT`
+  Paired-endsignature induced connection type.
+- `INFO/CIPOS`
+  Confidence interval around the start point of the SV.
+- `INFO/CIEND`
+  Confidence interval around the end point of the SV.
+- `INFO/SVMETHOD`
+  The name of the caller that was used.
+- `FORMAT` and per `SAMPLE`
+  - Common
+    - `GT` Genotype
+    - `FT` Per-genotype filter values
+    - `GQ` Phred-scaled genotype quality
+  - For Delly2
+    - `DR` Reference pairs
+    - `DV` Variant pairs
+    - `RR` Reference junction count
+    - `RV` Variant junction count
+  - For SV2
+    - `CN` Copy number estimate
+    - `PE` Normalized discordant paired-read count
+    - `SR` Normalized split read count
+    - `NS` Number of SNVs in the locus
+    - `HA` Heterozygous allele ratio
+    - `SQ` Phred-scaled genotype likelyhood
+  - For XHMM
+    - `DQ` Diploid Quality
+    - `NDQ` Non-diploid Quality
+    - `RD` Mean normalized read depth over region
+    - `PL` Genotype likelihoods for [diploid, deletion, duplication]
+  - For GATK gCNV
+     - `CN` Copy number
+     - `NP` Number of points in segment
+     - `QA` Phred-scale quality of all points agreeing
+     - `QS` Phred-scaled quality of least one point agreeing
+     - `QSS` Phred-scaled quality of start breakpoint
+     - `QSE` Phred-scaled quality of end breakpoint
+
 ## Example
 
 The following will create `varfish-annotator-db-1906.h2.db` and fill it.
---
+++ varfish-annotator-cli/src/test/java/com/github/bihealth/varfish_annotator/annotate_svs/RunWithRealWorldData37Test.java
@@ -0,0 +1,184 @@
+package com.github.bihealth.varfish_annotator.annotate_svs;
+
+import com.ginsberg.junit.exit.FailOnSystemExit;
+import com.github.bihealth.varfish_annotator.ResourceUtils;
+import com.github.bihealth.varfish_annotator.VarfishAnnotatorCli;
+import com.github.bihealth.varfish_annotator.utils.GzipUtil;
+import com.google.common.collect.Lists;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/** Test running with some real-world data files for GRCh37. */
+public class RunWithRealWorldData37Test {
+
+  @TempDir public File tmpFolder;
+  File h2DbFile;
+  File fastaFile;
+  File faiFile;
+  File ensemblSerFile;
+  File refseqSerFile;
+
+  @BeforeEach
+  void initEach() {
+    h2DbFile = new File(tmpFolder + "/small-GRCh38.h2.db");
+    fastaFile = new File(tmpFolder + "/chr22_part.fa");
+    faiFile = new File(tmpFolder + "/chr22_part.fa.fai");
+    ensemblSerFile = new File(tmpFolder + "/hg19_ensembl.ser");
+    refseqSerFile = new File(tmpFolder + "/hg19_refseq.ser");
+
+    ResourceUtils.gunzipResourceToFile("/grch37-chr22/chr22_part.fa.gz", fastaFile);
+    ResourceUtils.copyResourceToFile("/grch37-chr22/chr22_part.fa.fai", faiFile);
+    ResourceUtils.copyResourceToFile("/grch37-chr22/small-grch37-chr22.h2.db", h2DbFile);
+    ResourceUtils.copyResourceToFile("/grch37-chr22/hg19_ensembl.ser", ensemblSerFile);
+    ResourceUtils.copyResourceToFile("/grch37-chr22/hg19_refseq.ser", refseqSerFile);
+  }
+
+  void runTest(
+      String inputFileName,
+      String inputPath,
+      String expectedDbInfosFileName,
+      String expectedGtsFileName,
+      String expectedFeatureEffectsFileName,
+      boolean gzipOutput,
+      boolean selfTestChr1Only,
+      boolean selfTestChr22Only)
+      throws IOException {
+    final String gzSuffix = gzipOutput ? ".gz" : "";
+    final File vcfPath = new File(tmpFolder + "/" + inputFileName);
+    final File tbiPath = new File(vcfPath + ".tbi");
+    ResourceUtils.copyResourceToFile("/" + inputPath + "/" + vcfPath.getName(), vcfPath);
+    ResourceUtils.copyResourceToFile("/" + inputPath + "/" + tbiPath.getName(), tbiPath);
+
+    final File expectedDbInfosPath = new File(tmpFolder + "/" + expectedDbInfosFileName);
+    ResourceUtils.copyResourceToFile(
+        "/input/real-world-37/" + expectedDbInfosFileName, expectedDbInfosPath);
+    final File expectedGtsPath = new File(tmpFolder + "/" + expectedGtsFileName);
+    ResourceUtils.copyResourceToFile(
+        "/input/real-world-37/" + expectedGtsFileName, expectedGtsPath);
+    final File expectedFeatureEffectsPath =
+        new File(tmpFolder + "/" + expectedFeatureEffectsFileName);
+    ResourceUtils.copyResourceToFile(
+        "/input/real-world-37/" + expectedFeatureEffectsFileName, expectedFeatureEffectsPath);
+
+    final File outputDbInfoPath = new File(tmpFolder + "/output.db-info.tsv" + gzSuffix);
+    final File outputGtsPath = new File(tmpFolder + "/output.gts.tsv" + gzSuffix);
+    final File outputFeatureEffectsPath =
+        new File(tmpFolder + "/output.feature-effects.tsv" + gzSuffix);
+
+    final ArrayList<String> args =
+        Lists.newArrayList(
+            "annotate-svs",
+            "--release",
+            "GRCh37",
+            "--sequential-uuids",
+            "--db-path",
+            h2DbFile.toString(),
+            "--input-vcf",
+            vcfPath.toString(),
+            "--refseq-ser-path",
+            refseqSerFile.toString(),
+            "--ensembl-ser-path",
+            ensemblSerFile.toString(),
+            "--output-db-info",
+            outputDbInfoPath.toString(),
+            "--output-gts",
+            outputGtsPath.toString(),
+            "--output-feature-effects",
+            outputFeatureEffectsPath.toString());
+    if (selfTestChr1Only) {
+      args.add("--self-test-chr1-only");
+    } else if (selfTestChr22Only) {
+      args.add("--self-test-chr22-only");
+    }
+    final String[] argsArr = new String[args.size()];
+    args.toArray(argsArr);
+    VarfishAnnotatorCli.main(argsArr);
+
+    if (gzipOutput) {
+      final File paths[] = {outputDbInfoPath, outputGtsPath};
+      for (File path : paths) {
+        try (FileInputStream fin = new FileInputStream(path)) {
+          Assertions.assertTrue(GzipUtil.isGZipped(fin));
+        }
+      }
+    } else {
+      Assertions.assertEquals(
+          FileUtils.readFileToString(expectedDbInfosPath, "utf-8"),
+          FileUtils.readFileToString(outputDbInfoPath, "utf-8"));
+      Assertions.assertEquals(
+          FileUtils.readFileToString(expectedGtsPath, "utf-8"),
+          FileUtils.readFileToString(outputGtsPath, "utf-8"));
+      Assertions.assertEquals(
+          FileUtils.readFileToString(expectedFeatureEffectsPath, "utf-8"),
+          FileUtils.readFileToString(outputFeatureEffectsPath, "utf-8"));
+    }
+  }
+
+  @FailOnSystemExit
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void testDelly2(boolean gzipOutput) throws IOException {
+    runTest(
+        "bwa.delly2.Case_1_index-N1-DNA1-WGS1.vcf.gz",
+        "input/real-world-37",
+        "bwa.delly2.Case_1_index-N1-DNA1-WGS1.db-infos.tsv",
+        "bwa.delly2.Case_1_index-N1-DNA1-WGS1.gts.tsv",
+        "bwa.delly2.Case_1_index-N1-DNA1-WGS1.feature-effects.tsv",
+        gzipOutput,
+        false,
+        true);
+  }
+
+  @FailOnSystemExit
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void testGatkGcnv(boolean gzipOutput) throws IOException {
+    runTest(
+        "bwa.gcnv.NA12878-N1-DNA1-WGS1.vcf.gz",
+        "input/real-world-37",
+        "bwa.gcnv.NA12878-N1-DNA1-WGS1.db-infos.tsv",
+        "bwa.gcnv.NA12878-N1-DNA1-WGS1.gts.tsv",
+        "bwa.gcnv.NA12878-N1-DNA1-WGS1.feature-effects.tsv",
+        gzipOutput,
+        false,
+        true);
+  }
+
+  @FailOnSystemExit
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void testDragenCnv(boolean gzipOutput) throws IOException {
+    runTest(
+        "NA-12878WGS_dragen.cnv.vcf.gz",
+        "input/real-world-37",
+        "NA-12878WGS_dragen.cnv.db-infos.tsv",
+        "NA-12878WGS_dragen.cnv.gts.tsv",
+        "NA-12878WGS_dragen.cnv.feature-effects.tsv",
+        gzipOutput,
+        false,
+        true);
+  }
+
+  @FailOnSystemExit
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void testDragenSv(boolean gzipOutput) throws IOException {
+    runTest(
+        "NA-12878WGS_dragen.sv.vcf.gz",
+        "input/real-world-37",
+        "NA-12878WGS_dragen.sv.db-infos.tsv",
+        "NA-12878WGS_dragen.sv.gts.tsv",
+        "NA-12878WGS_dragen.sv.feature-effects.tsv",
+        gzipOutput,
+        false,
+        true);
+  }
+}
